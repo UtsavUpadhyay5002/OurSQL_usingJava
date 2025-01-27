b9a85c7ceb3bc98730d0f796469110b78f6f2a94
UtsavUpadhyay5002 <utsavupadhyay5002@gmail.com>
2025-01-26 21:07:39 -0400

    Update Query.java

--- Query.java
+++ Query.java
@@ -1,18 +1,29 @@
 
 public class Query 
 {
-    String queryType;
-    String command;
-
+    public static String curDatabase;
+    private String queryType;
+    private String command;
+    
+    //constructor
     public Query(String command)
     {
         queryType = command.substring(0, command.indexOf(' '));
         this.command = command;
+        curDatabase = "";
     }
     
+    //executor
     public void execute()
     {
-        if(queryType.equalsIgnoreCase("create") 
+        if(queryType.equalsIgnoreCase("show")) {
+            DatabaseManager.showDatabases();
+        }
+        else if (queryType.equalsIgnoreCase("use")) {
+            String dbName = command.substring(command.indexOf(" ")+1, command.indexOf(";"));
+            DatabaseManager.useDatabase(dbName);
+        }
+        else if(queryType.equalsIgnoreCase("create") 
             || queryType.equalsIgnoreCase("drop"))
         {
             DDLQuery ddlQuery= new DDLQuery(queryType, command);
@@ -23,7 +34,7 @@ public class Query
                     || queryType.equalsIgnoreCase("update")
                     || queryType.equalsIgnoreCase("delete"))
         {
-            DMLQuery dmlQuery = new DMLQuery(queryType, command);
+            DMLQuery dmlQuery = new DMLQuery(queryType, command, curDatabase);
             dmlQuery.execute();
         }
         else
@@ -31,4 +42,24 @@ public class Query
             System.out.println(String.format("ERROR: Query type (%s) is not recognized by this replics!!!", queryType));
         }
     }
+
+    //setter for fixed database when selected
+    public static void setDatabase(String dbname)
+    {
+        curDatabase = dbname;
+    }
+
+    //method to chck whether database is selected or not
+    public static boolean dbSelected()
+    {
+        if(curDatabase.equals(""))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
+
