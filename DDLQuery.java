bae480c7db7edab1dae18e17877b1846d0606e6c
UtsavUpadhyay5002 <utsavupadhyay5002@gmail.com>
2025-01-28 18:29:26 -0400

    Update DDLQuery.java
    added create and functionalities with respect to databases

--- DDLQuery.java
+++ DDLQuery.java
@@ -1,9 +1,7 @@
-
 //CREATE DATABASE DATABASE_NAME;
 //DROP DATABASE database_name;
 //CREATE TABLE TABLE_NAME(...); or CREATE TABLE TABLE_NAME (...);
 //DROP TABLE TABLE_NAME;
-
 
 public class DDLQuery 
 {
@@ -17,37 +15,59 @@ public class DDLQuery
         this.queryType = queryType;
         String[] parts = command.split(" ");
         fileType = parts[1];
-        if(parts[2].contains("("))
-        {
+        if(parts[2].contains("(")) {
             fileName = parts[2].substring(0,parts[2].indexOf("("));
         }
         else
         {
-            if(parts[2].contains(";"))
-            {
-                fileName = parts[2].substring(0,parts[2].length()-1);
+            if(parts[2].contains(";")) {
+                fileName = parts[2].replace(";", "");
             }
-            else
-            {
+            else {
                 fileName = parts[2];
             }
         }
-        if(command.contains("("))
-        {
+        if(command.contains("(")) {
             fields = command.substring(command.indexOf("(")+1, command.indexOf(")"));
         }
-        else
-        {
+        else {
             fields = "null";
         }
     }
 
     public void execute()
     {
-        System.out.println("DDL Query object contains:-");
-        System.out.println("queryType: " + queryType);
-        System.out.println("fileType: " + fileType);
-        System.out.println("fileName: " + fileName);
-        System.out.println("fields: " + fields);
+        if(queryType.equalsIgnoreCase("drop"))
+        {
+            //try-catch
+            if(fileType.equalsIgnoreCase("database")) {
+                DatabaseManager.dropDatabase(fileName);
+            }
+            else if(fileType.equalsIgnoreCase("table")) {
+                DatabaseManager.dropTable(Query.getDatabase(), fileName);
+            }
+            else {
+                System.out.println("ERROR: Inappropriate object Type: " + fileType);
+            }
+        }
+        else if(queryType.equalsIgnoreCase("create"))
+        {
+            //try-catch
+            if(fileType.equalsIgnoreCase("database")) {
+                DatabaseManager.createDatabase(fileName);
+            }
+            else if(fileType.equalsIgnoreCase("table")) {
+                //DatabaseManager.createTable(fileName); 
+                int i = 0;
+            }
+            else {
+                System.out.println("ERROR: Inappropriate object Type: " + fileType);
+            }
+        }
+        //System.out.println("DDL Query object contains:-");
+        //System.out.println("queryType: " + queryType);
+        //System.out.println("fileType: " + fileType);
+        //System.out.println("fileName: " + fileName);
+        //System.out.println("fields: " + fields);
     }
 }
